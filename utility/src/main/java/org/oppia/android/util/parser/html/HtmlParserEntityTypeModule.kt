935f66b44c5696e94a56df2fefd2f077069876d5
manas-yu <img_2022037@iiitm.ac.in>
2025-01-17 20:07:50 +0530

    dagger update

--- utility/src/main/java/org/oppia/android/util/parser/html/HtmlParserEntityTypeModule.kt
+++ utility/src/main/java/org/oppia/android/util/parser/html/HtmlParserEntityTypeModule.kt
@@ -2,10 +2,23 @@ package org.oppia.android.util.parser.html
 
 import dagger.Module
 import dagger.Provides
+import org.oppia.android.util.parser.html.ConceptCardTagHandler
+import javax.inject.Singleton
+import android.view.View
 
 /** Provides Html parsing entity type dependencies. */
 @Module
 class HtmlParserEntityTypeModule {
+  @Provides
+  @Singleton
+  fun provideConceptCardListener(): ConceptCardTagHandler.ConceptCardLinkClickListener {
+    return object : ConceptCardTagHandler.ConceptCardLinkClickListener {
+      override fun onConceptCardLinkClicked(view: View, skillId: String) {
+        // No-op implementation for tests
+      }
+    }
+  }
+
   @Provides
   @ExplorationHtmlParserEntityType
   fun provideExplorationHtmlParserEntityType(): String {
