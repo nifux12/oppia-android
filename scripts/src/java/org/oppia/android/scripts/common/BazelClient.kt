777516a16d48ec296fc212d54c7aa94721ce7754
manas-yu <img_2022037@iiitm.ac.in>
2025-07-07 20:10:39 +0530

    buil

--- scripts/src/java/org/oppia/android/scripts/common/BazelClient.kt
+++ scripts/src/java/org/oppia/android/scripts/common/BazelClient.kt
@@ -169,6 +169,11 @@ class BazelClient(private val rootDirectory: File, private val commandExecutor: 
     )
   }
 
+  /** Builds the specified Bazel target and returns the build output lines. */
+  fun buildTarget(bazelTarget: String): List<String> {
+    return executeBazelCommand("build", bazelTarget)
+  }
+
   /**
    * Returns Bazel workspace information.
    *
--- scripts/src/java/org/oppia/android/scripts/lint/AndroidLintRunner.kt
+++ scripts/src/java/org/oppia/android/scripts/lint/AndroidLintRunner.kt
@@ -108,6 +108,7 @@ class AndroidLintAnalyzer(
 
   /** Runs the complete lint analysis process. */
   fun runAnalysis() {
+    bazelClient.buildTarget("//:oppia_dev_binary")
     val logger = LintLogger(workingDirectory)
 //    logger.clearErrorLog()
     generateProjectDescription()
